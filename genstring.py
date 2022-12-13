617e5de72b070ba2fb96a5ab938f1c842a302f68
Dirganhere <98895216+EkooNihh@users.noreply.github.com>
2022-12-14 02:35:48 +0700

    Update genstring.py

--- genstring.py
+++ genstring.py
@@ -6,12 +6,19 @@
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
+
+
 import asyncio
+
 from pyrogram import Client as c
+
 API_ID = input("\nEnter Your API_ID:\n > ")
 API_HASH = input("\nEnter Your API_HASH:\n > ")
+
 print("\n\n Enter Phone number when asked.\n\n")
+
 i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
+
 async def main():
     await i.start()
     ss = await i.export_session_string()
